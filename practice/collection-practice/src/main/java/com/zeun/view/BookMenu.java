d08a287615145b2df3cc44499fd5569ffdc33511
YI JI EUN <ddzeun@gmail.com>
2025-05-13 17:41:42 +0900

    2025-05-13 일지 작성

---
+++ practice/collection-practice/src/main/java/com/zeun/view/BookMenu.java
@@ -0,0 +1,17 @@
+package com.zeun;
+
+import java.util.Scanner;
+
+public class BookMenu {
+
+    Scanner sc = new Scanner(System.in);
+    BookManager bm = new BookManager();
+
+    public BookMenu () {}
+
+    public void menu() {}
+
+    public BookDTO inputBook() {}
+
+    public String inputBookTitle() {}
+}
